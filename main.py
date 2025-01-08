65ffcf0b23f1bcd46557af73e3f4104976553077
pagal4206 <dilipjaat183@gmail.com>
2025-01-08 14:08:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from rudra.start import send_start_message
 from rudra.delete_media_edits import handle_media_edited_message
 from rudra.logging import log_bot_start, log_user_join, log_bot_added_to_group, log_bot_removed_from_group
 from rudra.user import get_group_count, get_user_count, add_group, add_user
-from warn import warn_user, handle_solution_button
+from rudra.warn import warn_user, handle_solution_button
 from rudra.broadcast import send_broadcast_message
 
 bot = telebot.TeleBot(BOT_TOKEN)
